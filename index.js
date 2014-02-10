d55a07653b06982c0b3af38de1fa5bbd6890756d
Hugh FD Jackson <hughfdjackson@googlemail.com>
2014-02-10 10:40:23 +0000

    Added `series`

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ var Q = require('q');
 'use strict';
 
 // Array[fn() -> Promise[T]] -> Promise[T]
-var fallback = function(arrayOfPromiseFn) {
+var series = function(promiseFns){ 
+    return promiseFns.reduce(function(promise, fn){ return promise.then(fn)}, Q());
+}
+
+// Array[fn() -> Promise[T]] -> Promise[T]
+var fallback = function(promiseFns) {
     var deferred = Q.defer();
 	var rejections = [];
     var tryNextPromise = function() {
-		if(arrayOfPromiseFn.length > 0) {
-			var first = arrayOfPromiseFn.shift();
+		if(promiseFns.length > 0) {
+			var first = promiseFns.shift();
 			first().then(function(result) {
 				deferred.resolve(result);
 			}, function(reason) {
@@ -25,6 +30,7 @@ var fallback = function(arrayOfPromiseFn) {
 
 module.exports = { 
 	object: require('./src/object'),
-	fallback: fallback
+	fallback: fallback,
+    series: series
 };
 
