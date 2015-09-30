c158c8713e648bd1b42afbeaf0ecc85a7b51779d
Jonathan Fielding <j.fielding@me.com>
2015-09-30 16:45:44 +0100

    Added unit tests to new All recursiveness

--- test/object-all-test.js
+++ test/object-all-test.js
@@ -29,4 +29,66 @@ describe('object.all', function(){
 		})
 		.then(done, done);
 	});
+
+	it('should reject if any of the nested values reject', function(done){
+		objectAll({ 
+			x: Q('foo'),
+			y: Q(),
+			z: {
+				a: Q.reject('la'),
+				b: Q('la'),
+				c: Q('la')
+			}
+		})
+		.then(Q.reject, function(err){ err.should.eql('la'); })
+		.then(done, done);
+	});
+
+	it('should resolve the full object tree values if all promises resolve', function(done){
+		objectAll({
+			x: Q('moo'),
+			y: Q('baa'),
+			z: {
+				a: Q('la'),
+				b: Q('la'),
+				c: Q('la')
+			}
+		})
+		.then(function(o){
+			o.should.eql({ 
+				x: 'moo',
+				y: 'baa',
+				z: {
+					a: 'la',
+					b: 'la',
+					c: 'la'
+				}
+			});
+		})
+		.then(done, done);
+	});
+
+	it('should resolve the full object tree including none promise values if all promises resolve', function(done){
+		objectAll({
+			x: 'moo',
+			y: Q('baa'),
+			z: {
+				a: Q('la'),
+				b: Q('la'),
+				c: 'la'
+			}
+		})
+		.then(function(o){
+			o.should.eql({ 
+				x: 'moo',
+				y: 'baa',
+				z: {
+					a: 'la',
+					b: 'la',
+					c: 'la'
+				}
+			});
+		})
+		.then(done, done);
+	});
 });
